21075e898abf87f8f88484958c0ab2146595945c
Alex Genzor <alexandros.genzor@fundacionmontessori.com>
2025-07-25 13:38:08 +0200

    (ignore any changes to Program.cs). Created Vehicle abstract class with all the needed properties and (pending) methods/functions to serve as a parent class.

--- AlexGenzor/EjercicioMasterD/Program.cs
+++ AlexGenzor/EjercicioMasterD/Program.cs
@@ -1,12 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using AlexGenzor.EjercicioMasterD.VehicleTypes;
+
 namespace AlexGenzor.EjercicioMasterD;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        
+        /*Vehicle vehicle = new Vehicle();
+        vehicle.EngineType = EngineTypes.Diesel;
+        Console.WriteLine(vehicle.ToString());*/
+
     }
     
 }
---
+++ AlexGenzor/EjercicioMasterD/VehicleTypes/Vehicle.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel;
+
+namespace AlexGenzor.EjercicioMasterD.VehicleTypes;
+
+public enum EngineTypes { Petrol, Diesel, Hybrid, Electric }
+public enum TractionTypes { AllWheels, FrontWheels, RearWheels }
+
+public abstract class Vehicle
+{
+    #region PROPERTIES
+    public string Model { get; set; }
+    public float HorsePower { get; set; }
+    public string PlateNumber { get; set; }
+    public int WheelCount { get; set; }
+    public EngineTypes EngineType { get; set; }
+    public string GearShiftType { get; set; }
+    public TractionTypes TractionType { get; set; }
+    
+    #endregion
+    
+    #region METHODS & FUNCTIONS
+
+    public virtual void AssignModel(string model)
+    {
+        
+        
+    }
+
+    public virtual void AssignHp()
+    {
+        
+        
+    }
+
+    public virtual void AssignWheelCount()
+    {
+        
+        
+    }
+    
+    public abstract void AssignEngineType();
+
+    public virtual void AssignShifterType()
+    {
+        
+        
+    }
+    
+    public abstract void AssignTractionType();
+    
+    public override string ToString()
+    {
+        return $"Model: {Model} - {PlateNumber}, {WheelCount} wheels, traction on {TractionType}" +
+               $"Engine type: {EngineType}, {HorsePower} HP. Gear shift type: {GearShiftType}";
+        
+    }
+
+    #endregion
+
+}
