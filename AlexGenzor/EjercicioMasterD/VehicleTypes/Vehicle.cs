6e5dbd365461567b98fa9a32df4001f9cce19ad5
Alex Genzor <alexandros.genzor@fundacionmontessori.com>
2025-07-28 13:01:14 +0200

    modified AssignTractionType() from abstract to virtual. All virtual functions have been completed.

--- AlexGenzor/EjercicioMasterD/VehicleTypes/Vehicle.cs
+++ AlexGenzor/EjercicioMasterD/VehicleTypes/Vehicle.cs
@@ -22,19 +22,35 @@ public abstract class Vehicle
 
     public virtual void AssignModel(string model)
     {
-        
+        Console.Write("Introduce el modelo del vehículo: ");
+        Model = Console.ReadLine();
         
     }
 
     public virtual void AssignHp()
     {
-        
-        
+        bool isValid = false;
+        float value;
+
+        while (!isValid)
+        {
+            Console.Write("¿Cuàntos caballos de potencia tiene el vehículo?: ");
+            if (float.TryParse(Console.ReadLine(), out value))
+            {
+                HorsePower = value;
+                isValid = true;
+                
+            }
+            else
+                Console.WriteLine("Valor introducido no válido. ");
+            
+        }
+
     }
 
     public virtual void AssignWheelCount()
     {
-        
+        WheelCount = 4;
         
     }
     
@@ -42,11 +58,71 @@ public abstract class Vehicle
 
     public virtual void AssignShifterType()
     {
+        NoValidShifter:
         
+        Console.Write("¿El vehículo tiene un cambio de marchas (1) Automático o (2) Manual?: ");
+        switch (Console.ReadLine())
+        {
+            case "Automatico":
+            case "automatico":
+            case "Auto":
+            case "auto":
+            case "1":
+                GearShiftType = "Automatico";
+
+                break;
+            
+            case "Manual":
+            case "manual":
+            case "2":
+                GearShiftType = "Manual";
+
+                break;
+            
+            default:
+                Console.WriteLine("Cambio de marchas introducido no válido.");
+                goto NoValidShifter;
+            
+        }
         
     }
-    
-    public abstract void AssignTractionType();
+
+    public virtual void AssignTractionType()
+    {
+        NoValidTraction:
+        
+        Console.Write("¿El vehículo es de (1) tracción delantera, (2) tracción trasera o (3) tracción total / 4x4?: ");
+
+        switch (Console.ReadLine())
+        {
+            case "Delantera":
+            case "delantera":
+            case "1":
+                TractionType = TractionTypes.FrontWheels;
+
+                break;
+            
+            case "Trasera":
+            case "trasera":
+            case "2":
+                TractionType = TractionTypes.RearWheels;
+
+                break;
+            
+            case "Total":
+            case "total":
+            case "3":
+                TractionType = TractionTypes.AllWheels;
+                
+                break;
+            
+            default:
+                Console.WriteLine("Tracción del vehículo introducida no válida. ");
+                goto NoValidTraction;
+            
+        }
+        
+    }
     
     public override string ToString()
     {
